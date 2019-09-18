01f5fffdec73a6ad29e0c035cddf8509ec95ad0a
rohan220217 <51409281+rohan220217@users.noreply.github.com>
2019-09-18 12:12:52 +0530

    Update Home_photo.py
    Changing object type to Json file

--- My first project/Home/Home_photo.py
+++ My first project/Home/Home_photo.py
@@ -1,22 +1,23 @@
 import requests
-from bs4 import BeautifulSoup
+import json
+from bs4 import BeautifulSoup 
 result = requests.get("http://midas.iiitd.edu.in/")
 src = result.content
 soup = BeautifulSoup(src, 'lxml')
 
-
 images = soup.find_all("img")
 for image in images:
-    print('http://midas.iiitd.edu.in/' + image['src'])
+    z = ( 'http://midas.iiitd.edu.in/' + image['src'])
+    y = {"image":z}
+    a = json.dumps(y)
+    print(a)
 
-
-
-
-http://midas.iiitd.edu.in//assets/themes/lab/images/logo/LOGO.png
-http://midas.iiitd.edu.in//assets/themes/lab/images/banner/banner.jpeg
-http://midas.iiitd.edu.in//assets/themes/lab/images/banner/group_1.jpeg
-http://midas.iiitd.edu.in//assets/themes/lab/images/banner/group.jpeg
-http://midas.iiitd.edu.in//assets/themes/lab/images/banner/group_2.jpeg
+    
+{"image": "http://midas.iiitd.edu.in//assets/themes/lab/images/logo/LOGO.png"}
+{"image": "http://midas.iiitd.edu.in//assets/themes/lab/images/banner/banner.jpeg"}
+{"image": "http://midas.iiitd.edu.in//assets/themes/lab/images/banner/group_1.jpeg"}
+{"image": "http://midas.iiitd.edu.in//assets/themes/lab/images/banner/group.jpeg"}
+{"image": "http://midas.iiitd.edu.in//assets/themes/lab/images/banner/group_2.jpeg"}
 
 
 
