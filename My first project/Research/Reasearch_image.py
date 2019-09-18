f0b5170fd5fc916c00f2c5aae96c6ba63a01ef8a
rohan220217 <51409281+rohan220217@users.noreply.github.com>
2019-09-18 12:22:46 +0530

    Update Reasearch_image.py
    Change object to json file

--- My first project/Research/Reasearch_image.py
+++ My first project/Research/Reasearch_image.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from bs4 import BeautifulSoup
 url = "http://midas.iiitd.edu.in/"
 result = requests.get(url)
@@ -15,19 +16,13 @@ for link in links:
         soup2 = BeautifulSoup(src2, 'lxml')
         images = soup2.find_all("img")
         for image in images:
-            print(url2 + image['src'])
-
-
-
-
-
-
-
-
-
-http://midas.iiitd.edu.in//projects//assets/themes/lab/images/logo/LOGO.png
-http://midas.iiitd.edu.in//projects//assets/images/projects/multimodal-social-media.jpg
-http://midas.iiitd.edu.in//projects//assets/images/projects/lipper.jpg
-http://midas.iiitd.edu.in//projects//assets/images/projects/ai-healtcare.jpg
-http://midas.iiitd.edu.in//projects//assets/images/projects/eventbuilder.jpg
-http://midas.iiitd.edu.in//projects//assets/images/projects/code-switched-language.jpg
+            a = (url2 + image['src'])
+            z = {"image": a}
+            y = json.dumps(z)
+            print(y)
+{"image": "http://midas.iiitd.edu.in//projects//assets/themes/lab/images/logo/LOGO.png"}
+{"image": "http://midas.iiitd.edu.in//projects//assets/images/projects/multimodal-social-media.jpg"}
+{"image": "http://midas.iiitd.edu.in//projects//assets/images/projects/lipper.jpg"}
+{"image": "http://midas.iiitd.edu.in//projects//assets/images/projects/ai-healtcare.jpg"}
+{"image": "http://midas.iiitd.edu.in//projects//assets/images/projects/eventbuilder.jpg"}
+{"image": "http://midas.iiitd.edu.in//projects//assets/images/projects/code-switched-language.jpg"}
